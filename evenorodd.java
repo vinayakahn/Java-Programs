a2aafcec4d5b936c7c0fd4e020ae85fc328aca29
Vinayaka H N <31912521+vinayakahn@users.noreply.github.com>
2018-10-16 02:20:33 -0700

    Declared a variable

--- evenorodd.java
+++ evenorodd.java
@@ -1,6 +1,10 @@
 Class EvenorOdd{
 public static void main(String args[])
 {
-System.out.println("Prints the Even Number");
+  int i=45;
+  if(i%2==0)
+  {
+System.out.println("Prints! this is a Even Number");
+  }
 }
 }
